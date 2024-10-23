93b5b54fda7bb0851571007a063e9532e9321a7d
Victor Alves <eleuteriovictor2@gmail.com>
2024-10-23 14:42:25 -0300

    alpha v1.1

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 // Verifica se o usuário está logado, caso contrário, redireciona para a tela de login
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     // Redireciona para a página de login com uma mensagem
-    header('Location: "/auth/login.php?error=not_logged_in"'); // Passando um parâmetro de erro
+    header('Location: auth/login.php?error=not_logged_in'); // Passando um parâmetro de erro
     exit;
 }
 ?>
@@ -15,6 +15,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Principal - Gerenciador do Jogo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
@@ -26,5 +27,8 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
         <p>Você está logado com sucesso!</p>
         <a href="auth/logout.php">Sair</a>
     </main>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="main.js"></script>
 </body>
 </html>
